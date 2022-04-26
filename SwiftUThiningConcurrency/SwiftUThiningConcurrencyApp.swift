d164f8cf39ed7caca67aff0aac38be28dc722419
jacobkosmart <jacobkosmart@gmail.com>
2022-04-26 09:13:16 +0900

    Initial Commit

---
+++ SwiftUThiningConcurrency/SwiftUThiningConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUThiningConcurrencyApp.swift
+//  SwiftUThiningConcurrency
+//
+//  Created by Jacob Ko on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUThiningConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
